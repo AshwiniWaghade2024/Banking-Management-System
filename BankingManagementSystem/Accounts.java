90c52aa9aa25afedce934623feae0739665861c7
AshwiniWaghade2024 <158551421+AshwiniWaghade2024@users.noreply.github.com>
2024-08-02 11:37:52 +0100

    Add files via upload

---
+++ BankingManagementSystem/Accounts.java
@@ -0,0 +1,21 @@
+package BankingManagementSystem;
+
+public class Accounts {
+
+	public long openAccount(String email){
+		
+		return 0;//temporary
+	}
+	public long AccountNum(String email) {
+		
+		return 0;//temporary
+	}
+	public boolean generateAcountNum() {
+		
+		return false;//temporary
+	}
+	public boolean accountExist() {
+		
+		return false;//temporary
+	}
+}
